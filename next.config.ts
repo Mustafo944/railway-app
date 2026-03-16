ee87013e80fd3b664a3cbbe39a1696ccdd0b9edb
Mustafo <mustafo949@gmail.com>
2026-03-16 12:01:06 +0500

    fix: remove next-pwa, use manual PWA

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {},
+};
 
-export default withPWA(nextConfig);
+export default nextConfig;
